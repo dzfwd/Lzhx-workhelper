7caade7015558d65ff3dd29ff0a3b0bde8a4f9a6
dzfwd <dzfwd@vip.qq.com>
2018-12-11 22:01:59 +0800

    更新了resume

--- Lzhx-workhelper/src/main/java/com/lzhx/entity/resume.java
+++ Lzhx-workhelper/src/main/java/com/lzhx/entity/resume.java
@@ -20,4 +20,123 @@ public class resume {
     private String wechatorqq;
     private String salary;
 
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getGraduated() {
+        return graduated;
+    }
+
+    public void setGraduated(String graduated) {
+        this.graduated = graduated;
+    }
+
+    public String getGtime() {
+        return gtime;
+    }
+
+    public void setGtime(String gtime) {
+        this.gtime = gtime;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public String getIDcardNo() {
+        return IDcardNo;
+    }
+
+    public void setIDcardNo(String IDcardNo) {
+        this.IDcardNo = IDcardNo;
+    }
+
+    public String getNativeplace() {
+        return nativeplace;
+    }
+
+    public void setNativeplace(String nativeplace) {
+        this.nativeplace = nativeplace;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWechatorqq() {
+        return wechatorqq;
+    }
+
+    public void setWechatorqq(String wechatorqq) {
+        this.wechatorqq = wechatorqq;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
 }
