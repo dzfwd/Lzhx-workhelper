545763dcbe0cec94fe2901cfc6f41ada3b6b0523
dzfwd <dzfwd@vip.qq.com>
2018-12-11 17:48:53 +0800

    测试GitHub

---
+++ Lzhx-workhelper/src/main/java/com/lzhx/entity/resume.java
@@ -0,0 +1,23 @@
+package com.lzhx.entity;
+
+/**
+ * Created by Administrator on 2018/12/11 0011.
+ */
+public class resume {
+    private String rid;
+    private String state;
+    private String uid;
+    private String sex;
+    private String birthdate;
+    private String phone;
+    private String education;
+    private String graduated;
+    private String gtime;
+    private String nation;
+    private String IDcardNo;
+    private String nativeplace;
+    private String address;
+    private String wechatorqq;
+    private String salary;
+
+}
